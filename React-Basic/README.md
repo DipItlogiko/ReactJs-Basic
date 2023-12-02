567ac7618e9d8f69443e1453b216b4eb81612ab3
DipItlogiko <dipitlogiko@gmail.com>
2023-12-02 19:11:57 +0600

    part-1

--- React-Basic/README.md
+++ React-Basic/README.md
@@ -1,24 +1,6 @@
-# JSX_Extension
+# CSS Styling in JSX
 
-jsx(java script XML) ai jsx syntax aa amra amader ReactJs ar code likhi...
-
-amader protita ReactJs ar file ar sheshe .jsx extension likhi...
-
-jsx syntax ta hocche HTML ar moto dekhte kintu jsx HTML na....amra ai .jsx extension  ar karone amader javascript code ar moddhe HTML likhte pari and jokhon amara  .jsx file ar moddhe react module ke import kori jemon ```import React form 'react';```  tokhon amader  ai react module ar moddhe akta compiler ache ```bable``` name oi compiler ta amader HTML take javascript a convart kore nicche...
-
-
-* amader .jsx file ar moddhe jokhon render korbo tokhon amader akta jinish mathai rakhte hobe ja amara amader .jsx file ar render ar moddhe akta element kei pass korte parbo jemon jodi ami ai akta element jemon ```<h1>hello</h1>```   likhi tahole amader render ai akta element take print korbe amader browser ar moddhe kintu jodi ami render ar moddhe 2 ta element ba tar theke beshi element likhi tahole amader oi render function ta amader browser ar moddhe oi ta print korbe na jemon jodi aivabe 2ta element likhi
-```<h1>hello</h1>```
-
-```<h2>hello</h2>```
-
-
-jodi amra render function ar moddhe 2ta element ba ar theke beshi element likhi tahole amader oobossoi oi element guloke ```<div></div>```  ba  ```<></>``` ar moddhe rakhte hobe tahole render function ta amader oi element guloke render korbe...
-
-Check src/main.jsx
-
-
-* jokhon amra .jsx file ar moddhe HTML ar moddhe kono JavaScript ar code likhbo ba javascript ar kono variable ar value ke print korbo tokhon amader {} ar  moddhe likhte hobe ....
+ 
 
 
 
--- React-Basic/src/main.jsx
+++ React-Basic/src/main.jsx
@@ -3,23 +3,30 @@ import ReactDOM from 'react-dom/client'
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));   //// amader render korar aage amader ai element gulo jei HTML page ar moddhe giye render hobe oi page ar moddhe amader jei id ba class ta ahche mane oi HTML page ar moddhe ai jei jaiga take id ba class ar akta nam diye define kore diyechi oi id ar nam ba class ar nam diye ami akta root create korechi amader oi HTML page ar moddhe oi define kora id ba class porjonto jawar jonno amra aikhane akta route create korechi... 
-
 const title = 'Home Page';   //// akhane ami javascript ar akta variable define korechi title nam aaa and ai title ar value hocche Home Page
-
 const date = new Date;
 const fullDate = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate(); 
+
+
+const custom_style = { 
+    backgroundColor: 'purple',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: '5rem',    
+}
+
+
 
 root.render(
   // <h1>Welcome</h1>          ////ami jodi render ar moddhe aivabe 2 ta elemetn likhi tahole amader render function akta error throw korbe karon amader render method ta akta element receive kore kintu amara aikhane 2 ta element likhechi tai amra amader render function ar moddhe 2ta ba tar beshi element ooo pass korte pari kintu oi element guloke amader <div></div> ba <></> ar moddhe rakhte hobe tahole amader render function ta amader browser ar moddhe oi element guloke render kore debe
   // <h2>dip</h2>
 
-
   <div>             {/* jokhon amader render function ar moddhe oonek gulo element thakbe tokhon oi element guloke amader <div></div> ba <></>  ar moddhe rakhte hobe */}
     <h1>{title}</h1>              {/* akhane ami amader javascript ar title variable ar value take print koriyechi */}
-    <h2>Welcome</h2>
-    <h3>dip</h3>
+    <h3 style={{color : 'red', fontSize : '3rem'}}>dip</h3>              {/* jokhon amra amader .jsx file ar moddhe style korbo tokhon amader sytle ar pore = dye amader {} dite hobe karon ami aikhane css use korchi tai and ai {} ar moddhe amader aabar akta {} mane curly braces dite hobe karon amader ai css gulo akta object aakare thakbe tai.. and amader CSS ar moddhe jai tag gulo emon font-size chilo oi guloke amader ai .jsx file ar moddhe ai vabe likhte hobe fontSize (camol case format aa) mane amader second letter ta capital latter aaa hobe and aita hocche amader inline CSS*/}
+    <h2 style={custom_style}>Welcome</h2>       {/* amra aivabe ooo amader style take add kore dite pari amader style take akta JavaScript variable  ar moddhe store kore then oi variable ar nam ta amader {} ai curly braces ar moddhe likhe diyechi */}
     <h3>{fullDate}</h3>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores? Corporis perspiciatis, officiis cupiditate ipsum minus sapiente, pariatur, expedita consequatur nisi voluptates nesciunt facere doloremque amet reprehenderit sed nemo asperiores.</p>
+    <p className='custom_css'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores?</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam inventore porro in expedita delectus harum ea veritatis dolorem quisquam doloremque corporis ut optio, minus quidem soluta officiis distinctio aspernatur molestiae!</p>
   </div>
 );
