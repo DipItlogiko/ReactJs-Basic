d5b8a4cf777fcc4aa3f83c335b67deb55d9b0840
DipItlogiko <dipitlogiko@gmail.com>
2023-12-03 10:26:47 +0600

    done

--- React-Basic/README.md
+++ React-Basic/README.md
@@ -1,6 +1,13 @@
 # CSS Styling in JSX
 
- 
+ * amra amader .jsx(java script XML) file ar moddhe in-line CSS internal CSS and External CSS oooo korte pari...kintu amader External CSS ar khetre akta bishoy amade mathai rakhte hobe je External CSS ar file ta jodi amra amder public directory ar moddhe create kori tahole amader oi external CSS file take  amder jekono html file ar moddhe link kore diye then oi html file ar sathe jei .jsx file ta add kora ache oi file ar moddhe giye amader oi external CSS ar class gulo likhle amader oi external CSS ar class ar style gulo amra amader browser ar moddhe dekhte pabo...
+
+ kintu jodi amra amader external CSS file ta amader ReactJs project ar src directory ar moddhe create kori tahole amader .html file ar moddhe link diye add korar dorkar nai kintu amra jei khane amader oi external CSS ar class gulo use korbo oi .jsx file ar moddhe amader ai src directory ar moddhe jei external CSS file ta ache oi file take amader .jsx file ar moddhe import kore nite hobe 
+
+
+ * mot kotha hocche je amra jodi kono external CSS file ke amader ReactJs Project ar moddhe public directory ar moddhe create kori tahole amader oi CSS file take amader .html file ar moddhe link koriye dite hobe tahole amader .html file ar sathe je .jsx file ta add kora ache oi file ar  moddhe amra oi external CSS ar class gulo use korte parbo
+
+ and jodi amra amader ReactJs project ar src directory ar moddhe amader external CSS file ta create kori tahole  amader .html file ar moddhe amder oi external CSS file take link korano lagbe na kintu amader oi .html file ar sathe jei .jsx file ta add ache oi .jsx file ar moddhe giye amader src directory ar moddhe jei external CSS file ta amra add korechi oi file take amader .jsx file ar moddhe import kore dite hobe..
 
 
 
--- React-Basic/src/main.jsx
+++ React-Basic/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './custom.css'
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));   //// amader render korar aage amader ai element gulo jei HTML page ar moddhe giye render hobe oi page ar moddhe amader jei id ba class ta ahche mane oi HTML page ar moddhe ai jei jaiga take id ba class ar akta nam diye define kore diyechi oi id ar nam ba class ar nam diye ami akta root create korechi amader oi HTML page ar moddhe oi define kora id ba class porjonto jawar jonno amra aikhane akta route create korechi... 
@@ -7,7 +8,7 @@ const title = 'Home Page';   //// akhane ami javascript ar akta variable define 
 const date = new Date;
 const fullDate = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate(); 
 
-
+//internel CSS
 const custom_style = { 
     backgroundColor: 'purple',
     color: 'white',
@@ -23,10 +24,10 @@ root.render(
 
   <div>             {/* jokhon amader render function ar moddhe oonek gulo element thakbe tokhon oi element guloke amader <div></div> ba <></>  ar moddhe rakhte hobe */}
     <h1>{title}</h1>              {/* akhane ami amader javascript ar title variable ar value take print koriyechi */}
-    <h3 style={{color : 'red', fontSize : '3rem'}}>dip</h3>              {/* jokhon amra amader .jsx file ar moddhe style korbo tokhon amader sytle ar pore = dye amader {} dite hobe karon ami aikhane css use korchi tai and ai {} ar moddhe amader aabar akta {} mane curly braces dite hobe karon amader ai css gulo akta object aakare thakbe tai.. and amader CSS ar moddhe jai tag gulo emon font-size chilo oi guloke amader ai .jsx file ar moddhe ai vabe likhte hobe fontSize (camol case format aa) mane amader second letter ta capital latter aaa hobe and aita hocche amader inline CSS*/}
+    <h3 style={{color : 'red', fontSize : '3rem'}}>dip</h3>              {/* jokhon amra amader .jsx file ar moddhe style korbo tokhon amader sytle ar pore = dye amader {} dite hobe karon ami aikhane css use korchi tai and ai {} ar moddhe amader aabar akta {} mane curly braces dite hobe karon amader ai css gulo akta object aakare thakbe tai.. and amader CSS ar moddhe jai tag gulo emon font-size chilo oi guloke amader ai .jsx file ar moddhe ai vabe likhte hobe fontSize (camol case format aa) mane amader second letter ta capital latter aaa hobe and aita hocche amader in-line CSS*/}
     <h2 style={custom_style}>Welcome</h2>       {/* amra aivabe ooo amader style take add kore dite pari amader style take akta JavaScript variable  ar moddhe store kore then oi variable ar nam ta amader {} ai curly braces ar moddhe likhe diyechi */}
     <h3>{fullDate}</h3>
-    <p className='custom_css'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores?</p>
+    <p className='External_css'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, asperiores?</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam inventore porro in expedita delectus harum ea veritatis dolorem quisquam doloremque corporis ut optio, minus quidem soluta officiis distinctio aspernatur molestiae!</p>
   </div>
 );
