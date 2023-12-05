aae4993f2137900e31b1db8edf8f41b893997963
DipItlogiko <dipitlogiko@gmail.com>
2023-12-05 18:36:05 +0600

    Done

--- ReactJs-project-template/src/components/Products.jsx
+++ ReactJs-project-template/src/components/Products.jsx
@@ -11,6 +11,7 @@ function Products(props) {
                     {Products.map((product) => (
                         <div key={product.id} className="col-md-4 col-lg-4 col-sm-12 mb-4">
                             <Product product={product} />
+                            {/*akhane amader Product component ta call hoyeche tai amader product component ta kar moddhe rakhbo oi digine ta akhane kore dite hobe ..*/}
                         </div>
                     ))}
                 </div>
