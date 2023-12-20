85d2235abe0dacd388f71c3059e3ff19bc716c26
DipItlogiko <dipitlogiko@gmail.com>
2023-12-20 11:51:25 +0600

    Done

--- ReactJs-project-template/README.md
+++ ReactJs-project-template/README.md
@@ -1,4 +1,4 @@
-# use ref in a class component with example ([Tutorial](https://www.youtube.com/watch?v=EyUsClvwmeo&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=54))
+# use ref in a functional component with example ([Tutorial](https://www.youtube.com/watch?v=l5z137GWakU&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=55))
 
 
 ```ref``` amader Directly Dom(Document Object Model) ke menupulate korte sahajjo kore.....Check(src/Components/UserForm.jsx)
--- ReactJs-project-template/src/Components/UserForm.jsx
+++ ReactJs-project-template/src/Components/UserForm.jsx
@@ -1,44 +1,44 @@
-/////========== Use Ref in Class Component with Example =============/////
-///NOTE: jemon ta amra dekhte parchi aita akta uncontrolled component karon amara amader form ar data guloke state ar maddhome handel na kore amra sora sori Ref ar maddhome form ar data gulo handel korchi..jei karone aita akta uncontrolled Component...amra sora sori aivabe korbo na mane amader form ar data gulo state chara handel korbo karon jodi amara aivabe kori mane Ref use kore sora sori Dom(Document Object Model) ke menupulate kori tahole amder performance issue hobe...amader uchit jotota somvob Dom(Document Object Model) ke sora sori menupulate na kora..
+/////========== Use Ref in Functional Component with Example =============/////
+///NOTE: jemon ta amra dekhte parchi aita akta uncontrolled component karon amara amader form ar data guloke state ar maddhome handel na kore amra sora sori useRef() hook ar maddhome form ar data gulo handel korchi..jei karone aita akta uncontrolled Component...amra sora sori aivabe korbo na mane amader form ar data gulo state chara handel korbo na karon jodi amara aivabe kori mane useRef hook use kore sora sori Dom(Document Object Model) ke menupulate kori tahole amder performance issue hobe...amader uchit jotota somvob Dom(Document Object Model) ke sora sori menupulate na kora..
 
-import React, { Component , createRef } from 'react' 
+import React,{ useRef } from 'react'
 
-class UserForm extends Component {
+function UserForm(){
 
-    constructor(props) {
-        super(props)
+    const userNameRef = useRef()  ////akhane ami amader userName input field ke useRef() hook use kore directly access korte chacchi tai useRef() hook take amder ai variable ar moddhe userNameRef store kore diyechi and ai variable take amder Dom(Document Object Model) ar element ar sathe connect kore diyechi niche
+    const passwordRef = useRef() 
 
-        this.userNameRef = createRef()  ////amra opore amder createRef ke import kore akhane amader userName input field ar jonno akta ref create korechi this.userNameRef ai nam aaa and ai variable ar moddhe amader createRef() method take define kore diyechi
 
-        this.state = {
+   const handleSubmit = (e) => { /////jokhon eee amder form ta submit hobe tokhon eeee amader onSubmit event ta fire hobe and oi onSubmit event take listen korbe amder ai handleSubmit function ba method ta and ai "e" variable ba paramiter ar moddhe amader event ta receive korbe
+     e.preventDefault()
+    //  console.log(userNameRef.current.value) /////akhane amader
+    //  console.log(passwordRef.current.value)
+    ////OR----
+    const Name = userNameRef.current.value
+    const password = passwordRef.current.value
+    console.log({Name , password})
 
-        }
+    userNameRef.current.style.backgroundColor = "red"  ////akhane amader ai userNameRef jei input field ar sathe connect kora ache jokon amader form ta submit hobe tar pore amder oi input file ar background color ta red hoye jabe
+    passwordRef.current.style.backgroundColor = "green"
+
     }
 
-    handleSubmit = (e) => {  //// jokhon eeee amader form ta submit hobe tokhon eee onSubmit event ta fire hobe and amder oi onSubmit event take listen korbe amader ai function ta...
-        e.preventDefault() ///submit howar sathe sathe browser ta bydefault vabe je reload ney oi take bondho kore diyechi
-        console.log(this.userNameRef.current.value)  //// amder userName field ar moddhe jokhon kew kono value diye submit korbe tokhon oi userName field ar value take amra console ar moddhe dekhchi
-        this.userNameRef.current.style.backgroundColor = "red"  ////amra chaile amader oi userName field take style ooo kore dite pari jemon ta amra korechi jokhon amder form ta submit hobe tar pore amader userName input field ar background color red hoye jabe...and amra chaile amader ai handleSubmit method ar moddhe amader form ar input field gulor jonno validation ooo korte pari ai Ref use kore
-    }
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-field">
-                        <label htmlFor="userName">UserName :</label>
-                        <input type="text" name="" id="userName" ref={this.userNameRef} />  {/* akhane ref={this.userNameRef} aita diye amader this.userNameRef jeita ami opore create korechi tar sathe amader ai input field take connect kore diyechi */}
-                    </div>
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="userName">UserName: </label>
+                    <input type="text" id="userName" ref={userNameRef} />  {/* akhane amder ai ref={userNameRef} ar maddhome amader ref take connect kore diyechi  */}
+                </div>
 
-                    <div className="form-field">
-                        <label htmlFor="password">Password :</label>
-                        <input type="password" name="" id="password" />
-                    </div>
-
-                    <button type='submit'>Register</button>
-                </form>
-            </div>
-        )
-    }
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="text" id="password" ref={passwordRef} />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
 }
 
 export default UserForm
