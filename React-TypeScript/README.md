8b6b65ccfc368c5f978146f7d75fcec7f4c8fb1b
DipItlogiko <dipitlogiko@gmail.com>
2024-01-06 16:54:58 +0600

    Done

--- React-TypeScript/README.md
+++ React-TypeScript/README.md
@@ -1,7 +1,11 @@
-# React-TypeScript|built_in_PropsTypes ([Tutorial](https://www.youtube.com/watch?v=-LRg9gC2ZB0&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=91))
+# React-TypeScript|user_defined_PropsTypes([Tutorial1](https://www.youtube.com/watch?v=2Lbb6wvvAIk&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=92))
 
 
 ```Props``` ar maddhome amra akta component ar moddhe theke r akta component ar moddhe data pass korte pari.... jemon ta amra ReactJs project ar moddhe korechilam parent component ar moddhe theke child component ar moddhe Props drilling ar maddhome data pass korechi
+
+* **builtIn** PropsTypes hocche jemon -> string, number , boolean, void, null
+
+* **userDefined** PropsTypes hocche jemon -> object,array,enum,union,any,custom type
 
 Note: jokhon amra ReactJs project ar moddhe TypeScript use korbo tokhon amader Component ar file gulor seshe .tsx extension ta add kore dite hobe tsx mane hocche **Type Script Xml** 
 
--- React-TypeScript/src/App.tsx
+++ React-TypeScript/src/App.tsx
@@ -1,6 +1,37 @@
+/////======= UserDefined PropsType Example with Array =========//////
+
+// import React from 'react' 
+
+// import User from './components/User'
+
+// function App() {
+ 
+
+//   return (    
+//       <div style={{ textAlign: "center" }}>
+//         <h1>React-TypeScript</h1>
+
+//         <User name="dip" age={20} isRegistered={false} lang={["Bangla" , "English" , "Hindi"]} />  {/* akhane amader User component take call kore props aakare kichu data pass korechi amder User component ar moddhe....and amader protita component file ar seshe .tsx diyechi karon ami ReactJs ar sathe TypeScript use korchi tai component ar sathe .tsx extention ta add kore diyechi tsx mane hocche type script xml */}
+        
+//       </div>
+//   )
+// }
+
+// export default App
+
+
+/////======= UserDefined PropsType Example with Object =========//////
+
 import React from 'react' 
 
 import User from './components/User'
+
+const user1 = { /////akhane ami akta object create korechi and ai object ar moddhe name,age,isRegistered,lang ai key gulor moddhe kichu data rekhechi and ai object take user1 variable ar moddhe store kore diyechi
+  name :"dip",
+  age : 20 ,
+  isRegistered : false ,
+  lang: ["Bangla" , "English" , "Hindi"],
+}
 
 function App() {
  
@@ -9,8 +40,8 @@ function App() {
       <div style={{ textAlign: "center" }}>
         <h1>React-TypeScript</h1>
 
-        <User name="dip" age={20} isRegistered={false} />  {/* akhane amader User component take call kore props aakare kichu data pass korechi amder User component ar moddhe....and amader protita component file ar seshe .tsx diyechi karon ami ReactJs ar sathe TypeScript use korchi tai component ar sathe .tsx extention ta add kore diyechi tsx mane hocche type script xml */}
-        <User name="dibbo" age={17} isRegistered={true} />
+        <User user={user1} />  {/* akhane amader User component take call kore props aakare amader user key ar moddhe user1 object take pass kore diyechi amder User component ar moddhe....and amader protita component file ar seshe .tsx diyechi karon ami ReactJs ar sathe TypeScript use korchi tai component ar sathe .tsx extention ta add kore diyechi tsx mane hocche type script xml ta chara jokhon amra jokhon kono object pass korbo kono component ar moddhe tokhon jei key ar moddhe amader object ta pass korbo oi key ar opore hover korle amader oi object ar moddhe jai key gulo ache oi key gulo soho amader key ar value gulor dataType ba PropsType ta dekhabe amra chile key ar opore hover kore amader object tar key ar dataType ba PropsType gulo copy kore niye amader oi component ar moddhe ai copy kor object ar datatype ba propstype gulo past kore dite pari jei component ar moddhe amra ai object take pass korchi oi component ar moddhe */}
+        
       </div>
   )
 }
